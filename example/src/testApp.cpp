2d4eb1d6d291a9080180f01181016992fed59b7f
George Profenza <orgicus@gmail.com>
2015-04-01 18:17:28 +0000

    updated with VBO Mesh(works on GL_ES

--- example/src/testApp.cpp
+++ example/src/testApp.cpp
@@ -1,60 +1,40 @@
 #include "testApp.h"
 
-bool exploding = false;
-int counter = 0;
 //--------------------------------------------------------------
 void testApp::setup(){
+	ofBackground(0);
 	model.read("torus.stl");
-	GLfloat LightAmbient[]= { 0.5f, 0.5f, 0.5f, 1.0f };
-	GLfloat LightDiffuse[]= { 1.0f, 1.0f, 1.0f, 1.0f };
-	GLfloat LightPosition[]= { 0.0f, 0.0f, 0.0f, 1.0f };
+	ofSetColor(255,255,255);
 	
-	glShadeModel(GL_SMOOTH);
-	glLightfv(GL_LIGHT1, GL_AMBIENT, LightAmbient);
-	glLightfv(GL_LIGHT1, GL_DIFFUSE, LightDiffuse);
-	glLightfv(GL_LIGHT1, GL_POSITION,LightPosition);
-	glEnable(GL_LIGHT1);
-	ofEnableSmoothing();
-	glEnable(GL_LIGHTING);
-	glEnable(GL_DEPTH_TEST);
-
-
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-	if(exploding) {
-		for(int i = 0; i < model.triangles.size(); i++) {
-			model.triangles[i].translate(ofRandom(-1,1), ofRandom(-1,1), ofRandom(-1,1));
-		}
-	}
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-	glPushMatrix();
-	glTranslatef(ofGetWidth()/2,ofGetHeight()/2, 0);
-	glScalef(10,10,10);
-	glRotatef(ofGetFrameNum(), 1,1,0);
-	model.draw();
-	glPopMatrix();
-	ofDrawBitmapString(ofToString(ofGetFrameRate(), 4)+ " FPS\nClick to explode the 3d model, press 's' to save out an STL file of the current geometry, press 'r' to reset the model", 10, ofGetHeight()-20);
+	ofPushMatrix();
+		ofTranslate(ofGetWidth()/2,ofGetHeight()/2, 0);
+		ofScale(10,10,10);
+		ofRotate(ofGetFrameNum(), 1,1,0);
+		if(wireframe){
+			model.vboMesh.drawWireframe();
+		}else{
+			model.draw();
+		}
+	ofPopMatrix();
+	ofDrawBitmapString(ofToString(ofGetFrameRate(), 4)+ " FPS", 10, ofGetHeight()-20);
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-	switch(key) {
-		case 's': model.write(ofToString(counter++) + ".stl"); break;
-		case 'r': 
-			exploding = false;
-			model.read("torus.stl"); 
-			break;
-	}
+	
 }
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){
-
+	wireframe = !wireframe;
 }
 
 //--------------------------------------------------------------
@@ -69,9 +49,7 @@ void testApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-	exploding = true;
 	
-
 }
 
 //--------------------------------------------------------------
